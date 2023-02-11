fbefbcc1d47ba2755911e416d6e112e443a20cb4
Nirosss <aniroiter@gmail.com>
2023-02-11 14:28:51 +0900

    synching versions

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,10 +1,10 @@
-<script setup>
+<script lang="ts">
   import { ref } from 'vue'
 </script>
 
 <template>
-  <header class="header-main full">
-    <div class="flex space-between">
+  <header class="header full">
+    <div class="header-container full flex space-between">
       <img src="../assets/images/AI TEXT (1).svg" alt="" class="logo-img" />
       <button class="header-btn">About</button>
     </div>
