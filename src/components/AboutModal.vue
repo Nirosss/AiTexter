c4ed51418b71c7c73b9c3d1b1fe699bc67ee0f24
Nirosss <aniroiter@gmail.com>
2023-02-21 12:01:08 +0200

    added modal content

--- src/components/AboutModal.vue
+++ src/components/AboutModal.vue
@@ -21,6 +21,36 @@
         <path d="m26 6-20 20"></path>
       </svg>
     </button>
-    <h2>This OpenAI tool was created by A.D and N.R</h2>
+    <div class="about-intro">
+    <h2>This OpenAI tool allows you to re-style any text you want</h2>
+    <p>
+      The converter uses a node.js server to deliver the request prompt to
+      OpenAI servers, it can be easily modified to perform any task ChatGpt is
+      capable of.
+    </p>
+    </div>
+    <div class="about-info">
+      <h4>To create it we used</h4>
+      <ul>
+        <li>Vue with composition API for the APP</li>
+        <li>TypeScript for the ###</li>
+        <li>Sass CSS for design</li>
+        <li>Node.js for the backend</li>
+        <li>Git to collaborate</li>
+      </ul>
+    </div>
+    <div class="about-us">
+    <div class="about-us img-container flex space-even">
+      <img
+  src="https://api.dicebear.com/5.x/big-ears/svg?seed=Avishai%20Dotan"
+  alt="avatar" />
+      <img
+  src="https://api.dicebear.com/5.x/big-ears/svg?seed=Nir%20Roiter"
+  alt="avatar" />
+    </div>
+    <div class="about-us txt-container">
+    <h5>Created by Avishai Dotan & Nir Roiter</h5>
+    </div>
+    </div>
   </div>
 </template>
